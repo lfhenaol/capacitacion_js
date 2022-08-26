e7e2e94d22b3c87b892d99843686894c71f2ed5d
lfhenaol <lfhenaolopez@misena.edu.co>
2022-08-26 00:06:47 -0500

    docs: agrega temas sobre destructuring

---
+++ 06_destructuring/index.js
@@ -0,0 +1,19 @@
+// Uso del operador rest en el destructuring
+
+// Destructuring en asignación de objetos
+
+// Destructuring en asigación de arrays
+
+// Destructuring anidado con objetos
+
+// Destructuring anidado con arrays
+
+// Renombrado de variables en el destructuring de objetos
+
+// Tricks
+
+// Swap
+
+// Omisión de elementos en un array
+
+// Configura valores por defecto en propiedades anidadas
