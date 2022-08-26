80958b707ce32006067820e3b3033115b670afa0
lfhenaol <lfhenaolopez@misena.edu.co>
2022-08-26 00:34:31 -0500

    docs: agrega temas sobre promesas

---
+++ 09_promesas/index.js
@@ -0,0 +1,10 @@
+// Creación de una promesa (obtención de valor eventualmente)
+
+// Extendiendo la creación de una promesa (promise, reject, resolve)
+
+// Encadenamiento de promesas (then y catch)
+
+// Uso de finally 
+
+
+
