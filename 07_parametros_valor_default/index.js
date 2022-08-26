3c21939173edd0e78c5cc9f2480affe6f4b64bcc
lfhenaol <lfhenaolopez@misena.edu.co>
2022-08-26 00:12:55 -0500

    docs: agrega temas sobre parámetros con valores default

---
+++ 07_parametros_valor_default/index.js
@@ -0,0 +1,5 @@
+// Parámetros de funciones con valores por defecto
+
+// Parámetros de funciones con destructuring y valores por defecto
+
+// Parámetros de funciones con destructuring y agrupamiento con valor por defecto
