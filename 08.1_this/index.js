503da07f5d08fd4a17dd5b554b68c5cf6a059709
lfhenaol <lfhenaolopez@misena.edu.co>
2022-08-26 00:25:17 -0500

    docs: agrega temas sobre this

---
+++ 08.1_this/index.js
@@ -0,0 +1,11 @@
+// Comportamiento del operador this dentro de funciones
+
+// Comportamiento del operador this dentro de métodos de clases
+
+// Compoetamiento del operador this dentro de objetos
+
+// Uso de la función bind para modificar el ambito de this
+
+// Uso de call par modificar el ambito de this (this,,,,,)
+
+// Uso de apply para mofificar el ambito de this (this,[,,,])
