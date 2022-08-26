8821d689a4f7c24343794826b64085459c2879e9
lfhenaol <lfhenaolopez@misena.edu.co>
2022-08-25 23:54:57 -0500

    docs: agrega temas sobre shorthands

--- 04_shorthands/index.js
+++ 04_shorthands/index.js
@@ -1,1 +1,15 @@
-//
+// Acceso a parámetros de una función empleando shorthand
+
+// Creación de objetos empleando shorthand
+
+// Creación de funciones dentro de objetos empleando shorthand
+
+// Retornando shorthands en funciones
+
+// Creación de propiedades en objetos empleando propiedades computadas
+
+// Evaluaciones con Operador de encadenamiento opcional
+
+// Evaluaciones con Nullish coalescing operator ("Operador de funsión nulo")
+
+// Evaluaciones con operadores lógicos
