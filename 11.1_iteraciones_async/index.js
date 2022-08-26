503f6ca90ddd58f77d3a0da77f11c601a83256f0
lfhenaol <lfhenaolopez@misena.edu.co>
2022-08-26 01:01:27 -0500

    docs: agrega temas sobre iteradores asíncronos

---
+++ 11.1_iteraciones_async/index.js
@@ -0,0 +1,16 @@
+// for await ..of
+
+// Generators, objetos de tipo iterable
+// Procesando grandes datos con generators https://www.youtube.com/watch?v=edaYw9UhQ0M
+
+/** 
+function* a() {
+  yield 1;
+  yield 3;
+}
+
+for await (const b of a()) {
+  console.log(b)
+}
+
+*/
