49622ab839e33d54b2b79be5fcbe6ef363d06a67
lfhenaol <lfhenaolopez@misena.edu.co>
2022-08-25 23:30:34 -0500

    docs: agrega los items a estudiar en objetos

---
+++ 02.1_objetos/index.js
@@ -0,0 +1,11 @@
+// Creación de objetos
+
+// Obtener claves de objetos
+
+// Obtener valores de objetos
+
+// Obtener entradas de objetos
+
+// Creación de objetos a partir de listas de entradas
+
+// Date y la librerñia date-fns(https://date-fns.org/)
